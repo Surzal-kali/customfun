50809f0586b5db4fe81bb4caf3729739cc7ca76e
Surzal-kali <vgreenwald@student.cnm.edu>
2026-07-16 20:21:39 -0600

    buffme

--- buffme.cpp
+++ buffme.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 //so this is definitely a buffer overflow vulnerability im so happy!!! THANK YOU POOKIE. I always wondered, how do you exploit this? (well, my first thought is file upload vulnerability, you look for exec. or compile as a "png!" or "exe" and upload it to the server)
 void vuln() {
-    char buffer[128||0];
+    char buffer[128];
     read(STDIN_FILENO, buffer, 512);
     printf("Buffer content: %s\n", buffer);
     if (strstr(buffer, "password")) { // Check if the buffer contains "password"
